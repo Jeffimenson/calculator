11ed8f59ced954a7ffdffce5d6a8b6a78a19cea9
Jeffimenson <jeffengim@gmail.com>
2023-06-11 20:07:11 -0400

    Make files and start on basic calculator layout and style

---
+++ program.js
@@ -0,0 +1,9 @@
+const numbers = document.querySelector('.numbers'); 
+
+// for (let i = 0; i < 10; i++){
+//     const button = document.createElement('button');
+//     button.classList.add('number');
+//     button.setAttribute('data-number', i); 
+//     button.textContent = i; 
+//     numbers.appendChild(button); 
+// } 
